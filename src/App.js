7df2fea1a74a90a67b596fab168ed1f27af5f370
Tom <tomhornbuckle1@gmail.com>
2020-01-31 01:44:16 +0000

    added weather, started on calandar component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import Header from './Components/Header/Header'
 import Weather from './Components/Weather/Weather'
+import Calendar from './Components/Calendar/Calendar'
 import './App.css'
-const { appKey } = require('./config.json')
+const { weatherKey } = require('./config.json')
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            description: '',
-            degrees: '',
-            city: '',
-            country: '',
             latitude: '',
-            longitude: ''
+            longitude: '',
+            description: 'Loading API...',
+            degrees: 'Loading API...',
+            city: 'Loading API...',
+            country: ''
         }
         this.fetchLocation()
-        this.fetchWeather()
-        
     }
+
     fetchLocation() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(this.getCoordinates)
@@ -27,45 +27,52 @@ class App extends React.Component {
         }
     }
 
-    getCoordinates = position => {
-        let latitude = position.coords.latitude.toFixed(3)
-        let longitude = position.coords.longitude.toFixed(3)
-        latitude = parseFloat(latitude)
-        longitude = parseFloat(longitude)
-        this.setState({
-            latitude: latitude,
-            longitude: longitude
-        })
+    getCoordinates = async position => {
+        return await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject)
+            console.log(position)
+            let latitude = position.coords.latitude.toFixed(3)
+            let longitude = position.coords.longitude.toFixed(3)
+            latitude = parseFloat(latitude)
+            longitude = parseFloat(longitude)
 
-        console.log(latitude + ' ' + longitude)
-        console.log(this.state)
+            this.setState({
+                latitude: latitude,
+                longitude: longitude
+            })
+            this.fetchWeather()
+            console.log(latitude + ' ' + longitude)
+            console.log(this.state)
+        })
     }
 
     fetchWeather = async () => {
-        let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&units=metric&appid=${appKey}`
-        console.log(weatherUrl)
+        let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&units=metric&appid=${weatherKey}`
         const apiCall = await fetch(weatherUrl)
         const weatherData = await apiCall.json()
         console.log(weatherData)
-        console.log(weatherData.weather['0'].icon)
+        console.log(this.state)
+        const intTemp = parseInt(weatherData.main.temp)
         this.setState({
             description: weatherData.weather['0'].description,
             city: weatherData.name,
             country: weatherData.sys.country,
-            degrees: weatherData.main.temp
+            degrees: intTemp
         })
+        console.log(weatherData)
     }
 
     render() {
         return (
-            <div className="Hompage">
+            <div className="Homepage">
+                <Calendar />
+                <Weather
+                    description={this.state.description}
+                    city={this.state.city}
+                    country={this.state.country}
+                    temp={this.state.degrees}
+                />
                 <div className="homepageHeader">
-                    <Weather
-                        description={this.state.description}
-                        city={this.state.city}
-                        country={this.state.country}
-                        temp={this.state.degrees}
-                    />
                     <Header />
                 </div>
             </div>
--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 const Weather = props => {
     return (
-        <div>
-            <h1>Weather</h1>
-            <p>{props.description}</p>
-            <p>
-                {props.city}, {props.country}
-            </p>
-            <p>{props.temp}&deg;C </p>
+        <div className="weatherContainer">
+            <div className="weather">
+                <h2>Weather</h2>
+                <div className="border"></div>
+                <h1>{props.temp}&deg;C </h1>
+                <p>{props.description}</p>
+                <p>
+                    {props.city} {props.country}
+                </p>
+            </div>
         </div>
     )
 }
