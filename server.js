a98b16abeacd65d9559074df523cf0529cea594b
Tom <tomhornbuckle1@gmail.com>
2020-02-05 17:26:14 +0000

    maked changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(__dirname, { dotfiles: 'allow' }))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
