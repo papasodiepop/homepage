42d60b4c2cd4874a3bc59cd41b782a4f362db12f
Tom <tomhornbuckle1@gmail.com>
2020-02-05 21:11:34 +0000

    changes again

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
 const privateKey = fs.readFileSync(
     '/etc/letsencrypt/live/tomhornbuckle.xyz/privkey.pem',
     'utf8'
@@ -26,11 +32,7 @@ const credentials = {
 
 const httpServer = http.createServer(app)
 const httpsServer = https.createServer(credentials, app)
-app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
 httpServer.listen(80, () => {
     console.log('HTTP Server running on port 80')
 })
