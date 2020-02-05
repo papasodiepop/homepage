de8880930dce319ec0efbdb1ada9df20f38e7f1d
Tom <tomhornbuckle1@gmail.com>
2020-02-05 22:12:49 +0000

    removed a line

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ class App extends React.Component {
         }
         this.fetchLocation()
     }
-
     fetchLocation() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(this.getCoordinates)
