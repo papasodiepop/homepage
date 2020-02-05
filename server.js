77fda4ac40d4a1d2026eae3af536a419ffc6cb1e
Tom <tomhornbuckle1@gmail.com>
2020-02-05 21:18:16 +0000

    making changes

--- server.js
+++ server.js
@@ -33,10 +33,6 @@ const credentials = {
 const httpServer = http.createServer(app)
 const httpsServer = https.createServer(credentials, app)
 
-httpServer.listen(80, () => {
-    console.log('HTTP Server running on port 80')
-})
-
 httpsServer.listen(443, () => {
     console.log('HTTPS Server running on port 443')
 })
