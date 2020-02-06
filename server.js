9587347e52b51ba35a89e840a842eeabfa102a0b
Tom <tomhornbuckle1@gmail.com>
2020-02-06 17:59:48 +0000

    testing

--- server.js
+++ server.js
@@ -5,24 +5,6 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'build')))
-app.enable('trust proxy')
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-app.get('*', (req, res) => {
-    res.redirect('https://' + req.headers.host + req.url)
-})
-app.use(function(req, res, next) {
-    if (req.secure) {
-        // https request, nothing to handle
-        next()
-    } else {
-        // this is an http request, redirect to https
-        res.redirect(301, 'https://' + req.headers.host + req.url)
-    }
-})
 const privateKey = fs.readFileSync(
     '/etc/letsencrypt/live/tomhornbuckle.xyz/privkey.pem',
     'utf8'
@@ -35,14 +17,20 @@ const ca = fs.readFileSync(
     '/etc/letsencrypt/live/tomhornbuckle.xyz/chain.pem',
     'utf8'
 )
-
 const credentials = {
     key: privateKey,
     cert: certificate,
     ca: ca
 }
+app.use(express.static(path.join(__dirname, 'build')))
 
-const httpServer = http.createServer(app)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+app.get('*', (req, res) => {
+    res.redirect('https://' + req.headers.host + req.url)
+})
+
 const httpsServer = https.createServer(credentials, app)
 
 httpsServer.listen(443, () => {
