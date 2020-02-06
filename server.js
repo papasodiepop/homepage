d3e079bc9a3b5705b27de690f0858e913d3dfb0d
Tom <tomhornbuckle1@gmail.com>
2020-02-06 17:47:42 +0000

    still testing

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.static(path.join(__dirname, 'build')))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
-app.get('*', function(req, res) {
-    res.redirect('https://tomhornbuckle.xyz')
+app.get('*', (req, res) => {
+    res.redirect('https://' + req.headers.host + req.url)
 })
 const privateKey = fs.readFileSync(
     '/etc/letsencrypt/live/tomhornbuckle.xyz/privkey.pem',
