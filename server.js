c16b48ea0daefd461cc65ddf882ca719c4620897
Tom <tomhornbuckle1@gmail.com>
2020-02-06 17:56:31 +0000

    server.js

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'build')))
 app.enable('trust proxy')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+app.get('*', (req, res) => {
+    res.redirect('https://' + req.headers.host + req.url)
+})
 app.use(function(req, res, next) {
     if (req.secure) {
         // https request, nothing to handle
@@ -15,12 +22,6 @@ app.use(function(req, res, next) {
         // this is an http request, redirect to https
         res.redirect(301, 'https://' + req.headers.host + req.url)
     }
-})
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-app.get('*', (req, res) => {
-    res.redirect('https://' + req.headers.host + req.url)
 })
 const privateKey = fs.readFileSync(
     '/etc/letsencrypt/live/tomhornbuckle.xyz/privkey.pem',
@@ -44,9 +45,6 @@ const credentials = {
 const httpServer = http.createServer(app)
 const httpsServer = https.createServer(credentials, app)
 
-httpServer.listen(80, () => {
-    console.log('HTTP Server running on port 80')
-})
 httpsServer.listen(443, () => {
     console.log('HTTPS Server running on port 443')
 })
